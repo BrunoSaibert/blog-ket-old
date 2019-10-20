e1bfdc1e26a9b31ae8a73fe21ded44cb0ce5dd08
BrunoSaibert <brunosaibert@gmail.com>
2019-10-19 21:44:54 -0300

    feature: Correção de publicação

---
+++ content/home.md
@@ -0,0 +1,6 @@
+---
+title: Primeiro post
+date: 2019-00-19T21:42:20.591Z
+---
+
+Teste
--- pages/index.js
+++ pages/index.js
@@ -5,20 +5,12 @@ export default class Home extends Component {
   render() {
     let {
       html,
-      attributes: { title, cats }
+      attributes: { title }
     } = content;
     return (
       <article>
         <h1>{title}</h1>
         <div dangerouslySetInnerHTML={{ __html: html }} />
-        <ul>
-          {cats.map((cat, k) => (
-            <li key={k}>
-              <h2>{cat.name}</h2>
-              <p>{cat.description}</p>
-            </li>
-          ))}
-        </ul>
       </article>
     );
   }

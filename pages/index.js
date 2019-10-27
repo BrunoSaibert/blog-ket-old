7ec1de4e424ded10c93d5398f1b8d39e97219fda
BrunoSaibert <brunosaibert@gmail.com>
2019-10-27 15:22:30 -0300

    fix: Estilo Post

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Intro from "../components/Intro";
 import Container from "../components/Container";
 
-import { PostList, PostItem } from "./styles";
+import { PostList, PostItem } from "../components/Post/styles";
 
 export default class pages extends Component {
   render() {

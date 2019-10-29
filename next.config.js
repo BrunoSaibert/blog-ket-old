35d2350645c16436bd3a4c342a8781a370a15acb
BrunoSaibert <brunosaibert@gmail.com>
2019-10-28 23:41:12 -0300

    Feature: Deploy

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ module.exports = {
       "/Contato": { page: "/Contato" }
     };
     // dynamic, data-generated pages
-    const content = await jdown("content"); // assumes some markdown files in a `/content` folder, with frontmatter that offers a slug
+    const content = await jdown("content/blog"); // assumes some markdown files in a `/content` folder, with frontmatter that offers a slug
     const posts = []; // build up array of objects for the top level list
     Object.entries(content).forEach(([filename, fileContent]) => {
       // the filename becomes the slug

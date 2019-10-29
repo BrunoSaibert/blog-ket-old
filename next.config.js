a7bc2a8419f06cc6662fcad1db68c52791c3e2c9
BrunoSaibert <brunosaibert@gmail.com>
2019-10-28 22:01:43 -0300

    fix: Props página de post

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
-module.exports = {
-  webpack: cfg => {
-    cfg.module.rules.push({
-      test: /\.md$/,
-      use: "frontmatter-markdown-loader"
-    });
-    return cfg;
-  }
-};
+// module.exports = {
+//   webpack: cfg => {
+//     cfg.module.rules.push({
+//       test: /\.md$/,
+//       use: "frontmatter-markdown-loader"
+//     });
+//     return cfg;
+//   }
+// };
+
+const exportPathMap = require("./exportPathMap");
+
+const withMDX = require("@zeit/next-mdx")({
+  extension: /.mdx?$/
+});
+
+module.exports = withMDX({
+  pageExtensions: ["js", "jsx", "mdx"],
+  exportPathMap
+});

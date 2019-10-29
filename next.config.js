82941b1b12e15df5ff9cc26cbdde933ed0edb64d
BrunoSaibert <brunosaibert@gmail.com>
2019-10-28 22:33:52 -0300

    fix: Deploy

--- next.config.js
+++ next.config.js
@@ -1,22 +1,4 @@
-// module.exports = {
-//   webpack: cfg => {
-//     cfg.module.rules.push({
-//       test: /\.md$/,
-//       use: "frontmatter-markdown-loader"
-//     });
-//     return cfg;
-//   }
-// };
-
-const exportPathMap = require("./exportPathMap");
-
-const withMDX = require("@zeit/next-mdx")({
-  extension: /.mdx?$/
-});
-
-module.exports = withMDX({
-  pageExtensions: ["js", "jsx", "mdx"],
-  exportPathMap,
+module.exports = {
   webpack: cfg => {
     cfg.module.rules.push({
       test: /\.md$/,
@@ -24,4 +6,4 @@ module.exports = withMDX({
     });
     return cfg;
   }
-});
+};
--- pages/Post/[slug].js
+++ pages/Post/[slug].js
@@ -22,14 +22,20 @@ class PostPage extends Component {
     const post = findDoc(this.props.slug);
     return (
       <>
-        <Intro
-          urlBackground="https://picsum.photos/id/409/2000/1000"
-          title={post.attributes.title}
-          subTitle={post.attributes.description}
-        />
-        <Container>
-          <article dangerouslySetInnerHTML={{ __html: post.html }}></article>
-        </Container>
+        {post && (
+          <>
+            <Intro
+              urlBackground="https://picsum.photos/id/409/2000/1000"
+              title={post.attributes.title}
+              subTitle={post.attributes.description}
+            />
+            <Container>
+              <article
+                dangerouslySetInnerHTML={{ __html: post.html }}
+              ></article>
+            </Container>
+          </>
+        )}
       </>
     );
   }
